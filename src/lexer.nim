3b2ef0928bbc769c6d1657ae4621016db320b215
Andres Lowrie <andres.lowrie@gmail.com>
2022-06-20 14:58:11 -0500

    feat: add basic tokens

--- src/lexer.nim
+++ src/lexer.nim
@@ -2,15 +2,62 @@
 
 type TokenType* = enum
   tkInvalid, tkEof, tkIdentifier, tkNewline, tkIndent, tkColon, tkAssign,
-  tkDollar, tkHyphen, tkUnderscore, tkQuote, tkBackslash, tkForwardslash,
-  tkLParen, tkRParen, tkLBrace, tkRBrace, tkLBracket, tkRBracket,
-  tkLAngleBracket, tkRAngleBracket, tkPipe, tkAmpersand, tkSemiColon, tkHash
+  tkDollar, tkHyphen, tkUnderscore, tkSingleQuote, tkDoubleQuote, tkBackslash,
+  tkForwardslash, tkLParen, tkRParen, tkLBrace, tkRBrace, tkLBracket,
+  tkRBracket, tkLAngleBracket, tkRAngleBracket, tkPipe, tkAmpersand,
+  tkSemiColon, tkHash
 
 type Token* = object
   tkType*: TokenType
 
-proc toToken*(input: string): Token =
+proc toToken*(input: char): Token =
   case input
-  of "":
-    result = Token(tkType: tkEof)
+  of '\n':
+    result = Token(tkType: tkNewline)
+  of '\t':
+    result = Token(tkType: tkIndent)
+  of ':':
+    result = Token(tkType: tkColon)
+  of '=':
+    result = Token(tkType: tkAssign)
+  of '$':
+    result = Token(tkType: tkDollar)
+  of '-':
+    result = Token(tkType: tkHyphen)
+  of '_':
+    result = Token(tkType: tkUnderscore)
+  of '\'':
+    result = Token(tkType: tkSingleQuote)
+  of '"':
+    result = Token(tkType: tkDoubleQuote)
+  of '\\':
+    result = Token(tkType: tkBackslash)
+  of '/':
+    result = Token(tkType: tkForwardslash)
+  of '(':
+    result = Token(tkType: tkLParen)
+  of ')':
+    result = Token(tkType: tkRParen)
+  of '{':
+    result = Token(tkType: tkLBrace)
+  of '}':
+    result = Token(tkType: tkRBrace)
+  of '[':
+    result = Token(tkType: tkLBracket)
+  of ']':
+    result = Token(tkType: tkRBracket)
+  of '<':
+    result = Token(tkType: tkLAngleBracket)
+  of '>':
+    result = Token(tkType: tkRAngleBracket)
+  of '|':
+    result = Token(tkType: tkPipe)
+  of '&':
+    result = Token(tkType: tkAmpersand)
+  of ';':
+    result = Token(tkType: tkSemiColon)
+  of '#':
+    result = Token(tkType: tkHash)
+  else:
+    result = Token(tkType: tkInvalid)
 
--- tests/tlexer.nim
+++ tests/tlexer.nim
@@ -1,4 +1,37 @@
 import ../src/lexer
 
-# Empty input should return EOF
-assert toToken("").tkType == tkEof
+# Empty input should return Invalid
+var input: char
+assert toToken(input).tkType == tkInvalid
+
+# Map tokens to their string representation
+let pairs = @[
+  (tkInvalid, '!'),
+  (tkNewline, '\n'),
+  (tkIndent, '\t'),
+  (tkColon, ':'),
+  (tkAssign, '='),
+  (tkDollar, '$'),
+  (tkHyphen, '-'),
+  (tkUnderscore, '_'),
+  (tkSingleQuote, '\''),
+  (tkDoubleQuote, '"'),
+  (tkBackslash, '\\'),
+  (tkForwardslash, '/'),
+  (tkLParen, '('),
+  (tkRParen, ')'),
+  (tkLBrace, '{'),
+  (tkRBrace, '}'),
+  (tkLBracket, '['),
+  (tkRBracket, ']'),
+  (tkLAngleBracket, '<'),
+  (tkRAngleBracket, '>'),
+  (tkPipe, '|'),
+  (tkAmpersand, '&'),
+  (tkSemiColon, ';'),
+  (tkHash, '#'),
+]
+for p in pairs:
+  let (t, s) = p
+  let got = toToken(s)
+  assert got.tkType == t, "Want: " & $t & " Got: " & $got.tkType
