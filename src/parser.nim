19bc89a116840cdb95301dfc104bc0e3e7256361
Andres Lowrie <andres.lowrie@gmail.com>
2022-10-22 13:01:28 -0500

    wip: make the ast deal in Nodes

--- src/parser.nim
+++ src/parser.nim
@@ -1,10 +1,31 @@
+import lexer
+
+# need to think about this.
+# Given that nim doesn't use "concepts" ie I can't have a "node" interface; I
+# need to see how I group the statements and expressions into a type so that
+# the Program type can operate on a single type
+#
+# perhaps this is where Generics come into play?
 type NodeType* = enum
   nStatement,
-  nExpresion
+  nExpression
 
 type Node* = object
-  nType: NodeType
-
+  nType*: NodeType
+  token*: Token
+# ###
 
 type Program* = object
-  statements: seq[Node]
+  statements*: seq[Node]
+
+type AssignStatement* = object
+  # don't know how to narrow these down...
+  name*: Node # ...should only be able to be TokenType.tkIdentifier
+  value*: Node # and this should only be NodeType.nExpression
+
+proc getTokenLiteral*(n: Node): string =
+  n.token.literal
+
+proc getTokenLiteral*(p: Program): string =
+  for n in p.statements:
+    add(result, getTokenLiteral(n))
--- tests/tparser.nim
+++ tests/tparser.nim
@@ -1,7 +1,23 @@
 import ../src/parser
+import ../src/lexer
 
 proc printAssumption(want, got: auto): string =
   result = "Want: " & $want & " Got: " & $got
 
 block: # Top-Level Suite
-  assert true == false
+  block: # getTokenLiteral
+    var tok = Token(literal: "a", tkType: tkIdentifier)
+    var nod = Node(nType: nExpression, token: tok)
+    let want = "a"
+    var got = getTokenLiteral(nod)
+    assert got == want, printAssumption(want, got)
+
+  block: # program
+    var tok1 = Token(literal: "a", tkType: tkIdentifier)
+    var tok2 = Token(literal: "b", tkType: tkIdentifier)
+    var nod1 = Node(nType: nExpression, token: tok1)
+    var nod2 = Node(nType: nExpression, token: tok2)
+    var prog = Program(statements: @[nod1, nod2])
+    let want = "ab"
+    var got = getTokenLiteral(prog)
+    assert got == want, printAssumption(want, got)
